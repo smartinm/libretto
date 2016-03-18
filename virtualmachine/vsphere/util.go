b422e34b5e5b6c045436825b4ec5045ced63e978
Preet Bhinder <preet@apcera.com>
2016-03-18 16:39:27 -0700

    vSphere: Retrieve network cards when a VM is found
    The API for vSphere was changed to get ips from network cards instead of
    the guest.IPaddress property so that all ips could be retrieved.
    However, that property needs to be retrieved from the API. Currently, it
    is empty.

--- virtualmachine/vsphere/util.go
+++ virtualmachine/vsphere/util.go
@@ -306,7 +306,7 @@ func searchTree(vm *VM, mor types.ManagedObjectReference, name string) (*mo.Virt
 	case "VirtualMachine":
 		// Base recursive case, compare for value
 		vmMo := mo.VirtualMachine{}
-		err := vm.collector.RetrieveOne(vm.ctx, mor, []string{"name", "guest.ipAddress", "guest.guestState"}, &vmMo)
+		err := vm.collector.RetrieveOne(vm.ctx, mor, []string{"name", "guest.ipAddress", "guest.guestState", "guest.net"}, &vmMo)
 		if err != nil {
 			return nil, NewErrorObjectNotFound(errors.New("could not find the vm"), name)
 		}
